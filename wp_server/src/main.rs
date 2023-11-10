5839eef4a282b1c53f7091bbd26cfb17c8531ce0
Quinn Parrott <quinn.parrott@gmail.com>
2023-11-10 14:48:33 -0500

    Moved client async task into own function

--- wp_server/src/main.rs
+++ wp_server/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::{BTreeMap, HashMap, VecDeque};
 use std::fs::File;
+use std::net::SocketAddr;
 use std::sync::{Arc, OnceLock};
 use std::time::{Duration, Instant};
 
@@ -187,6 +188,151 @@ impl<'a> Widget for LogBufferWidget<'a> {
 const PLAYBACK_START_BUF: Duration = Duration::from_millis(500);
 const FPS: f64 = 60.0;
 
+async fn client(
+    mut socket: TcpStream,
+    addr: SocketAddr,
+    start_time: Arc<Instant>,
+    current_video: Arc<RwLock<Option<(ServingVideo, State)>>>,
+    mut retransmit_channel: broadcast::Receiver<ServerMsg>,
+) -> Result<()> {
+    let mut magic_buf = [0; 4];
+    socket.read_exact(&mut magic_buf).await.unwrap();
+
+    if *MAGIC != magic_buf {
+        send_socket(&mut socket, ServerMsg::Error(format!("Bad magic number")))
+            .await
+            .unwrap();
+        panic!("Bad magic number {magic_buf:?}");
+    }
+
+    // let mut version_buf = [0; 4];
+    let version = socket.read_u32_le().await.unwrap();
+
+    if version != VERSION {
+        send_socket(
+            &mut socket,
+            ServerMsg::Error(format!("Unsupported version")),
+        )
+        .await
+        .unwrap();
+        panic!("Wrong version {version} (supported version {VERSION})");
+    }
+
+    info!("{}: Client connected", addr);
+
+    info!("{}: Syncing time...", addr);
+    let mut time_id_counter = 0;
+    let mut samples = vec![];
+    for _ in 0..30 {
+        send_socket(
+            &mut socket,
+            ServerMsg::RequestTime {
+                id: time_id_counter,
+            },
+        )
+        .await
+        .unwrap();
+        let time = Instant::now();
+
+        let size = socket.read_u32_le().await.unwrap();
+        let delta = time.elapsed();
+        let current_time = start_time.elapsed();
+
+        let mut buf = vec![0; size.try_into().unwrap()];
+        socket.read_exact(&mut buf).await.unwrap();
+        let res: ClientMsg = postcard::from_bytes(&mut buf).unwrap();
+        let ClientMsg::CurrentTime {
+            id: _,
+            unix_time_micro,
+        } = res;
+
+        let client_time = Duration::from_micros(unix_time_micro.try_into().unwrap());
+        let estimated_client_delta = current_time - (client_time + delta / 2);
+        // dbg!(delta, current_time, client_time, &res, estimated_client_delta);
+        samples.push((delta, estimated_client_delta));
+
+        time_id_counter += 1;
+    }
+
+    let samples = samples.into_iter().collect::<BTreeMap<_, _>>();
+    debug!("{:#?}", &samples);
+    let sample_count = samples.len();
+    let mut samples_iter = samples.into_iter().rev();
+    let (mut _last_error, mean) = samples_iter.next().unwrap();
+    let mut mean_time_delta = mean.as_secs_f64();
+    let mut error_sum = Duration::ZERO;
+    for (error, sample) in samples_iter {
+        error_sum += error;
+        // TODO: Measure this based on error somehow to be more statistically rigorous?
+        let sample_importance_ration = 0.25;
+        mean_time_delta = mean_time_delta * (sample_importance_ration)
+            + sample.as_secs_f64() * (1.0 - sample_importance_ration);
+    }
+    let greeting = format!(
+        "Time delta ({:?}), mean latency ({:?}), samples ({})",
+        Duration::from_secs_f64(mean_time_delta),
+        error_sum / (sample_count as u32),
+        sample_count
+    );
+    info!("{}: {}", addr, greeting);
+    send_socket(&mut socket, ServerMsg::Error(greeting))
+        .await
+        .unwrap();
+
+    let mean_time_delta = Duration::from_secs_f64(mean_time_delta);
+
+    {
+        if let Some((loaded_video, state)) = &*current_video.read().await {
+            let mut state = *state;
+            state.normalize_time(&start_time);
+            send_socket(&mut socket, Into::<ServerMsg>::into(loaded_video.clone()))
+                .await
+                .unwrap();
+            send_socket(
+                &mut socket,
+                match Into::<ServerMsg>::into(state) {
+                    ServerMsg::StartPlayingAt {
+                        unix_time_micro,
+                        playback_time_frames,
+                    } => {
+                        let client_time_micro =
+                            (Duration::from_micros(unix_time_micro.try_into().unwrap())
+                                - mean_time_delta)
+                                .as_micros();
+                        ServerMsg::StartPlayingAt {
+                            unix_time_micro: client_time_micro,
+                            playback_time_frames,
+                        }
+                    }
+                    other => other,
+                },
+            )
+            .await
+            .unwrap();
+        }
+    }
+
+    loop {
+        let command: ServerMsg = retransmit_channel.recv().await.unwrap();
+        let msg = match command {
+            ServerMsg::StartPlayingAt {
+                unix_time_micro,
+                playback_time_frames,
+            } => {
+                let client_time_micro =
+                    (Duration::from_micros(unix_time_micro.try_into().unwrap()) - mean_time_delta)
+                        .as_micros();
+                ServerMsg::StartPlayingAt {
+                    unix_time_micro: client_time_micro,
+                    playback_time_frames,
+                }
+            }
+            msg => msg,
+        };
+        send_socket(&mut socket, msg).await.unwrap();
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let (log_sender, mut log_receiver) = mpsc::unbounded_channel();
@@ -203,147 +349,24 @@ async fn main() -> Result<()> {
     let start_time_clients = start_time.clone();
     tokio::spawn(async move {
         loop {
-            let (mut socket, addr) = listener.accept().await.unwrap();
+            let (socket, addr) = listener.accept().await.unwrap();
 
-            let mut retransmit_channel = broadcast_rx.resubscribe();
+            let retransmit_channel = broadcast_rx.resubscribe();
 
             let start_time = start_time_clients.clone();
             let current_video = current_video_clients.clone();
-            tokio::spawn(async move {
-                let mut magic_buf = [0; 4];
-                socket.read_exact(&mut magic_buf).await.unwrap();
-
-                if *MAGIC != magic_buf {
-                    send_socket(&mut socket, ServerMsg::Error(format!("Bad magic number")))
-                        .await
-                        .unwrap();
-                    panic!("Bad magic number {magic_buf:?}");
-                }
-
-                // let mut version_buf = [0; 4];
-                let version = socket.read_u32_le().await.unwrap();
-
-                if version != VERSION {
-                    send_socket(
-                        &mut socket,
-                        ServerMsg::Error(format!("Unsupported version")),
-                    )
-                    .await
-                    .unwrap();
-                    panic!("Wrong version {version} (supported version {VERSION})");
-                }
-
-                info!("{}: Client connected", addr);
-
-                info!("{}: Syncing time...", addr);
-                let mut time_id_counter = 0;
-                let mut samples = vec![];
-                for _ in 0..30 {
-                    send_socket(
-                        &mut socket,
-                        ServerMsg::RequestTime {
-                            id: time_id_counter,
-                        },
-                    )
-                    .await
-                    .unwrap();
-                    let time = Instant::now();
-
-                    let size = socket.read_u32_le().await.unwrap();
-                    let delta = time.elapsed();
-                    let current_time = start_time.elapsed();
-
-                    let mut buf = vec![0; size.try_into().unwrap()];
-                    socket.read_exact(&mut buf).await.unwrap();
-                    let res: ClientMsg = postcard::from_bytes(&mut buf).unwrap();
-                    let ClientMsg::CurrentTime {
-                        id: _,
-                        unix_time_micro,
-                    } = res;
-
-                    let client_time = Duration::from_micros(unix_time_micro.try_into().unwrap());
-                    let estimated_client_delta = current_time - (client_time + delta / 2);
-                    // dbg!(delta, current_time, client_time, &res, estimated_client_delta);
-                    samples.push((delta, estimated_client_delta));
-
-                    time_id_counter += 1;
-                }
-
-                let samples = samples.into_iter().collect::<BTreeMap<_, _>>();
-                debug!("{:#?}", &samples);
-                let sample_count = samples.len();
-                let mut samples_iter = samples.into_iter().rev();
-                let (mut _last_error, mean) = samples_iter.next().unwrap();
-                let mut mean_time_delta = mean.as_secs_f64();
-                let mut error_sum = Duration::ZERO;
-                for (error, sample) in samples_iter {
-                    error_sum += error;
-                    // TODO: Measure this based on error somehow to be more statistically rigorous?
-                    let sample_importance_ration = 0.25;
-                    mean_time_delta = mean_time_delta * (sample_importance_ration)
-                        + sample.as_secs_f64() * (1.0 - sample_importance_ration);
-                }
-                let greeting = format!(
-                    "Time delta ({:?}), mean latency ({:?}), samples ({})",
-                    Duration::from_secs_f64(mean_time_delta),
-                    error_sum / (sample_count as u32),
-                    sample_count
-                );
-                info!("{}: {}", addr, greeting);
-                send_socket(&mut socket, ServerMsg::Error(greeting)).await.unwrap();
-
-                let mean_time_delta = Duration::from_secs_f64(mean_time_delta);
-
-                {
-                    if let Some((loaded_video, state)) = &*current_video.read().await {
-                        let mut state = *state;
-                        state.normalize_time(&start_time);
-                        send_socket(&mut socket, Into::<ServerMsg>::into(loaded_video.clone()))
-                            .await
-                            .unwrap();
-                        send_socket(
-                            &mut socket,
-                            match Into::<ServerMsg>::into(state) {
-                                ServerMsg::StartPlayingAt {
-                                    unix_time_micro,
-                                    playback_time_frames,
-                                } => {
-                                    let client_time_micro = (Duration::from_micros(
-                                        unix_time_micro.try_into().unwrap(),
-                                    ) - mean_time_delta)
-                                        .as_micros();
-                                    ServerMsg::StartPlayingAt {
-                                        unix_time_micro: client_time_micro,
-                                        playback_time_frames,
-                                    }
-                                }
-                                other => other,
-                            },
-                        )
-                        .await
-                        .unwrap();
-                    }
-                }
-
-                loop {
-                    let command: ServerMsg = retransmit_channel.recv().await.unwrap();
-                    let msg = match command {
-                        ServerMsg::StartPlayingAt {
-                            unix_time_micro,
-                            playback_time_frames,
-                        } => {
-                            let client_time_micro =
-                                (Duration::from_micros(unix_time_micro.try_into().unwrap())
-                                    - mean_time_delta)
-                                    .as_micros();
-                            ServerMsg::StartPlayingAt {
-                                unix_time_micro: client_time_micro,
-                                playback_time_frames,
-                            }
-                        }
-                        msg => msg,
-                    };
-                    send_socket(&mut socket, msg).await.unwrap();
+            tokio::task::spawn(async move {
+                let res = tokio::spawn(client(
+                    socket,
+                    addr,
+                    start_time,
+                    current_video,
+                    retransmit_channel.resubscribe(),
+                ));
+                match res.await {
+                    Ok(Ok(())) => {}
+                    Ok(Err(err)) => error!("{}: Task error: {}", addr, err),
+                    Err(err) => error!("{}: Task failed: {}", addr, err),
                 }
             });
         }
